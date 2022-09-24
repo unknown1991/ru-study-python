0883316bee0fca841979d2d21075d9cbc1ea8c6b
Ramazanov <b.g.ramazanov@gmail.com>
2022-09-24 16:54:33 +0300

    solved the tasks from map.py

--- exercises/map.py
+++ exercises/map.py
@@ -12,7 +12,25 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
+<<<<<<< Updated upstream
         pass
+=======
+
+        rating_sum = 0.0
+        movies_count = 0
+
+        for movie in list_of_movies:
+            country_count = movie["country"].count(",") + 1
+            movie_rating = movie["rating_kinopoisk"]
+
+            if country_count > 1 and movie_rating != "" and float(movie_rating) > 0:
+                rating_sum += float(movie["rating_kinopoisk"])
+                movies_count += 1
+
+        average_rating = rating_sum / movies_count
+
+        return average_rating
+>>>>>>> Stashed changes
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -26,4 +44,15 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
+<<<<<<< Updated upstream
         pass
+=======
+
+        filtered_list_of_movies = (
+            movie["name"]
+            for movie in list_of_movies
+            if movie["rating_kinopoisk"] != "" and float(movie["rating_kinopoisk"]) >= rating
+        )
+
+        return sum(map(lambda name: name.count("и"), filtered_list_of_movies))
+>>>>>>> Stashed changes
